8912aea96e9d8c1263e8e05affda3994b3b8b718
Rojel Perez Vez <rojelpv@gmail.com>
2020-04-25 11:54:41 -0400

    Updates

---
+++ precache-manifest.7937ce117eae90308819c09813824108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f62b9a0f29467aedab3317332940243",
+    "url": "/EmployeeDirectory/index.html"
+  },
+  {
+    "revision": "c11fe14048f4707285d7",
+    "url": "/EmployeeDirectory/static/css/main.2eefddad.chunk.css"
+  },
+  {
+    "revision": "cf9185e7cf7269f4ebbb",
+    "url": "/EmployeeDirectory/static/js/2.33601343.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeDirectory/static/js/2.33601343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c11fe14048f4707285d7",
+    "url": "/EmployeeDirectory/static/js/main.f82948ef.chunk.js"
+  },
+  {
+    "revision": "8985a01cbf9e8a96c7ad",
+    "url": "/EmployeeDirectory/static/js/runtime-main.9dda3b4d.js"
+  }
+]);
